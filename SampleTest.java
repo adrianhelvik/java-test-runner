5aaf0189928efdd31da3a7d670f30ad21eb88734
Adrian <adrianhelvik100@gmail.com>
2016-02-16 03:07:42 +0100

    Prettier and handier syntax

--- SampleTest.java
+++ SampleTest.java
@@ -3,33 +3,34 @@ import com.adrianhelvik.testrunner.*;
 class SampleTest extends TestCase {
 
     public static void main(String[] args) {
-        describe("A suite")
-            .it("contains a spec with an expectation")
-            .expect(true).toBe(true);
+        describe("A suite");
 
-        describe("toBe")
-            .it("tests for equality with the == operator")
-            .expect(10).toBe(10)
-            .expect(new String("Hello")).not.toBe(new String("Helloh".substring( 0, "Helloh".length() - 1 )));
+        it("contains a spec with an expectation");
+        expect(true).toBe(true);
 
-        describe("toEqual")
-            .it("tests for equality with the .equals method and should work for object references")
-            .expect(new String("Hello")).toEqual(new String("Hello"))
-            .it("should also work for primitives")
-            .expect(1337).toEqual(1337);
+        describe("toBe");
+        it("tests for equality with the == operator");
+        expect(10).toBe(10);
+        expect(new String("Hello")).not.toBe(new String("Helloh".substring( 0, "Helloh".length() - 1 )));
 
-        describe("Timing a call is easy...")
-            .it("just call time() before a test.")
-            .time()
-            .expect(true).toBe(true)
-            .it("but maybe you don't want to test anything, just test the execution time of a function.")
-            .time()
-            .call("Hello world".matches("world"))
-            .it("and calling the method without timinging it will just not display any time")
-            .call("Hello world".matches("world"));
+        describe("toEqual");
+        it("tests for equality with the .equals method and should work for object references");
+        expect(new String("Hello")).toEqual(new String("Hello"));
+        it("should also work for primitives");
+        expect(1337).toEqual(1337);
 
-        describe("Comparing arrays")
-            .it("should pass with toEqual if the arrays are equivalent")
-            .expect( new int[] { 1, 2, 3 } ).toEqual( new int[] { 1, 2, 3 } );
+        // describe("Timing a call is easy...");
+        // it("just call time() before a test.");
+        // time();
+        // expect(true).toBe(true);
+        // it("but maybe you don't want to test anything, just test the execution time of a function.");
+        // time();
+        // call("Hello world".matches("world"));
+        // it("and calling the method without timinging it will just not display any time");
+        // call("Hello world".matches("world"));
+
+        describe("Comparing arrays");
+        it("should pass with toEqual if the arrays are equivalent");
+        expect( new int[] { 1, 2, 3 } ).toEqual( new int[] { 1, 2, 3 } );
     }
 }
--- com/adrianhelvik/testrunner/TestCase.java
+++ com/adrianhelvik/testrunner/TestCase.java
@@ -3,11 +3,30 @@ import com.adrianhelvik.testrunner.*;
 
 public class TestCase {
 
+    public static TestDescription currentDescription = null;
+    public static Assertion currentAssertion = null;
+
     //
     // Testing boilerplate
     //
 
-    public static TestDescription describe(String description) {
-        return new TestDescription(description);
+    public static void describe( String description ) {
+        currentDescription = new TestDescription(description);
+    }
+
+    public static void it( String shouldDo ) {
+        currentAssertion = currentDescription.it( shouldDo );
+    }
+
+    public static void xit( String shouldDo ) {
+        currentAssertion = currentDescription.xit( shouldDo );
+    }
+
+    public static Expectation expect( Object value ) {
+        return currentAssertion.expect( value );
+    }
+
+    public static Expectation xexpect( Object value ) {
+        return currentAssertion.expect( value );
     }
 }
